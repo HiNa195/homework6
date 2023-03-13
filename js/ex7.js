92eddc3e948c30555729187031f63e33c7bcec0d
Khanh Linh Tran <KhanhLinh.Tran01@student.csulb.edu>
2023-03-13 16:32:43 -0700

    1st Release

--- js/ex7.js
+++ js/ex7.js
@@ -3,16 +3,35 @@
    -- User Input / Sum
 */
 
-function add()
-{
-   const x = document.getElementById("content");
-   let y = document.createElement("input");
-   y.setAttribute("type", "number");
-   let z = document.createElement("input");
-   z.setAttribute("type", "number");
-   let sum = 0;
-   sum.textContent = y + z;
-   console.log(sum);
-   x.value = sum;
-}
-add();
+// Pseudocode
+// Input (hard code)
+const n1 = prompt("Please enter the first integer");
+const n2 = prompt("Please enter the second integer");
+
+// Need validation
+
+// Calculate sum
+const sum = Number(n1) + Number(n2);
+
+// Output
+const paraElement = document.createElement("p");
+// Add the text to the paragraph
+// using innerHTML, innerText, textContent, add a text node...
+paraElement.innerText = `${n1} + ${n2} = `;
+// Create span element with the sum and red font
+const spanElement = document.createElement("span");
+spanElement.textContent = sum;
+spanElement.style.color = "red";
+spanElement.style.fontWeight = "bold";
+// Add span to the paragraph
+paraElement.appendChild(spanElement);
+// Add the paragraph to the document
+const divElement = document.getElementById("content");
+divElement.appendChild(paraElement);
+
+/*
+// Alternate way using innerHTML
+const paraElement2 = document.createElement("p");
+paraElement2.innerHTML = `${n1} + ${n2} = <span style="color:red; font-weight:bold"> ${sum}</span>`;
+divElement.appendChild(paraElement2);
+*/
